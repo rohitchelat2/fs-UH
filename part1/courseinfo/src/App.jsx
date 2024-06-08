18bfc3693a07d5671c587dadab5a41c852223732
rohitchelat2 <157241551+rohitchelat2@users.noreply.github.com>
2024-06-08 22:38:09 +0300

    one single object

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [{ name: 'Fundamentals of React', exercises: 10},{ name: 'Using props to pass data', exercises: 7}, { name: 'State of a component', exercises: 14}]
-
-
+  const course ={ name: 'Half Stack application development',
+     parts: [{ name: 'Fundamentals of React', exercises: 10},
+    { name: 'Using props to pass data', exercises: 7}, 
+    { name: 'State of a component', exercises: 14}]}
+  
   return (
     <div>
-      <Head course={course}/>
-      <Content part1={parts[0]} part2={parts[1]} part3={parts[2]}/>
-      <Total  exercises1={parts[0].exercises} exercises2={parts[1].exercises} exercises3={parts[2].exercises} />
+      <Head course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total  parts={course.parts}/>
       
      </div>
   )
@@ -25,11 +26,10 @@ const Head = (props) => {
 }
 
 const Content = (props) => {
+
   return (
     <div>
-      <Parts name={props.part1.name} exercises={props.part1.exercises}/>
-      <Parts name={props.part2.name} exercises={props.part2.exercises}/>
-      <Parts name={props.part3.name} exercises={props.part3.exercises}/>
+      {props.parts.map(value => <Parts name={value.name} exercises={value.exercises} />)}
     </div>
   )
 }
@@ -38,7 +38,7 @@ const Content = (props) => {
 const Total = (props) => {
   return (
     <div>
-    <p> Number of exercises = {props.exercises1+props.exercises2+props.exercises3} </p>
+    <p> Number of exercises = {props.parts[0].exercises+props.parts[1].exercises+props.parts[2].exercises} </p>
     </div>
   )
 }
