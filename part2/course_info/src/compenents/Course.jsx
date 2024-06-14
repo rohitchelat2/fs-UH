dcdc8bf008a9667e3d1d560150170db63f39898c
rohitchelat2 <157241551+rohitchelat2@users.noreply.github.com>
2024-06-14 19:27:26 +0300

    course__info_part2 final commit

---
+++ part2/course_info/src/compenents/Course.jsx
@@ -0,0 +1,32 @@
+const Header = (props) => <h1>{props.name}</h1>
+
+const Total = (props) => <p><b>Number of exercises {props.sum}</b></p>
+
+const Part = (props) => {
+  const part=props.part;
+  return(
+  <p key={part.id}> {part.name} {part.exercises} </p>)}
+
+const Content = (props) => { 
+  const parts = props.parts
+ 
+  const total = parts.map(part => part.exercises).reduce((s, a) => s + a, 0)
+
+  return(
+  <div >
+    {parts.map(part => <Part key={part.id} part={part}/>)}
+    <Total sum={total}/>
+  </div> )
+}
+
+const Course = (props) => {
+  const courses = props.courses
+  return(
+ <div> 
+  {courses.map(course => (<div key={course.id}>
+  <Header name={course.name} />
+  <Content parts={course.parts} /></div>))}
+ </div>
+)}
+
+export default Course
